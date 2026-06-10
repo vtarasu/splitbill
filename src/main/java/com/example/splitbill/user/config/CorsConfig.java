1acc593609431a95ea010489a66042f88b3fee55
vtarasu <vthirunavukkarasu96@gmail.com>
2026-06-11 00:16:23 +0530

    cors config added as config

--- src/main/java/com/example/splitbill/user/config/CorsConfig.java
+++ src/main/java/com/example/splitbill/user/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.splitbill.user.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,12 +10,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin(allowedOrigins);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
